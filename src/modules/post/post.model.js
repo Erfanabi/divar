0a44fa52a3d4af6b41ce692b2f31f2ef4cc1bf30
Erfan Sharafi <erfansharafi60@gmail.com>
2025-02-17 18:28:07 +0330

    post model

---
+++ src/modules/post/post.model.js
@@ -0,0 +1,22 @@
+const { Schema, Types, model } = require("mongoose");
+
+const PostSchema = new Schema({
+  title: { type: String, required: true }, // عنوان آگهی
+  userId: { type: Types.ObjectId, required: true }, // شناسه کاربر
+  amount: { type: Number, required: true, default: 0 }, // مقدار یا قیمت
+  content: { type: String, required: true }, // محتوای آگهی
+  category: { type: Types.ObjectId, ref: "Category", required: true }, // دسته‌بندی آگهی
+  province: { type: String, required: false }, // استان
+  city: { type: String, required: false }, // شهر
+  district: { type: String, required: false }, // منطقه
+  address: { type: String, required: false }, // آدرس
+  coordinate: { type: [Number], required: true }, // مختصات جغرافیایی
+  images: { type: [String], required: false, default: [] }, // تصاویر آگهی
+  options: { type: Object, default: {} } // گزینه‌های اضافی
+}, {
+  timestamps: true // زمان‌بندی ایجاد و به‌روزرسانی
+});
+
+const PostModel = model("Post", PostSchema);
+
+module.exports = PostModel;
